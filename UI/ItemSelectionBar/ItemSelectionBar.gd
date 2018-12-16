ffc5f20a705e071dbf9367baf19aa73878aed023
JanisMancevics <splintercdo@gmail.com>
2018-12-16 08:07:59 +0200

    Shader in progrss, just for insurance a commit

--- UI/ItemSelectionBar/ItemSelectionBar.gd
+++ UI/ItemSelectionBar/ItemSelectionBar.gd
@@ -138,6 +138,17 @@ func _process(delta):
 	else:
 		manage_bar_visibility(.0, original_y_position + get_viewport().size.y, delta * BAR_ALPHA_DISSAPPEAR_SPEED, delta)
 
+	var previous_transparency_coefficient = .0 # For speed and convenience.
+	for i in range(0, level_selection_bars[Global.current_level_index].size()):
+		var current_item = level_selection_bars[Global.current_level_index][i][0] # For speed and convenience.
+		if current_item.alpha_must_be_managed:
+			var current_transparency_coefficient = ITEM_ALPHA_TRANSITION_HARSHNESS * ((rect_position.x + current_item.rect_position.x) / get_viewport().size.x) # For speed and convenience.
+			current_item.material.set_shader_param("previous_transparency_coefficient", previous_transparency_coefficient)
+			current_item.material.set_shader_param("current_transparency_coefficient", current_transparency_coefficient)
+			previous_transparency_coefficient = current_transparency_coefficient
+
+const ITEM_ALPHA_TRANSITION_HARSHNESS = 7.0; # How close to the side of the screen transparency must start.
+
 func _physics_process(delta):
 	if bar_shrink_items.size() > 0:
 		var loop_range = level_selection_bars[Global.current_level_index].size() - (bar_shrink_items[0] + 1) # To know how many items to move.
